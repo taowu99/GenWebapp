544cc0d4c936c0b1c93e1c6620ada62302fbd41c
taowu99 <taowu99@gmail.com>
2016-09-06 21:16:52 -0400

    version of security login, implemented with jsp and sprint security

---
+++ src/main/java/org/tao/generic/web/GenWebapp/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package org.tao.generic.web.GenWebapp.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+	public SecurityWebApplicationInitializer() {
+		super(SecurityConfig.class);
+	}
+}
